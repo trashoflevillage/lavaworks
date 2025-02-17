31be7c8cbc32a1cdf04d115b9a264242d58f156a
trashoflevillage <uhhnokthxbye@gmail.com>
2025-02-17 13:13:05 -0500

    An attempt at colored magma blocks (does NOT work)

--- src/main/java/io/github/trashoflevillage/lavaworks/LavaWorksClient.java
+++ src/main/java/io/github/trashoflevillage/lavaworks/LavaWorksClient.java
@@ -3,10 +3,18 @@ package io.github.trashoflevillage.lavaworks;
 import eu.midnightdust.lib.config.MidnightConfig;
 import io.github.trashoflevillage.lavaworks.config.LavaWorksConfig;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.color.block.BlockColors;
+import net.minecraft.client.color.world.BiomeColors;
+import net.minecraft.world.biome.FoliageColors;
 
 public class LavaWorksClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockColors blockColors = new BlockColors();
+        blockColors.registerColorProvider(
+                (state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.getDefaultColor(),
+                Blocks.MAGMA_BLOCK
+        );
     }
 }
